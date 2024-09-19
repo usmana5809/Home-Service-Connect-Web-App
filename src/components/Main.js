c5692653b7aaf189b5c10956acdecefd7fb2bad2
Usmana Zulfiqar <118796976+usmana5809@users.noreply.github.com>
2024-09-19 23:45:18 +0500

    resposive the my web app

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,17 +9,18 @@ function Main() {
       <section className="container1" >
         <div className="container-fluid main-container">
           <div className="row" >
-            <div className="col-md-6" data-aos="fade-right" data-aos-duration="2000"><br/>
+            <div className="col-md-5" data-aos="fade-right" data-aos-duration="2000"><br/>
               <h1><b>Welcome to AuraUserve</b></h1><br/><br/>
               <h5><i>Your Trusted Home and Clinical Services Partner</i></h5>
               <p>Welcome to AuraUserve, where convenience meets care. Elevate your lifestyle with our seamless home services and comprehensive clinical solutions. Whether you're looking for a skilled service provider for your home or seeking top-notch clinical care, AuraUserve is your go-to destination.</p>
              <br/><br/>
               <button type="button" className="btn btn-dark">Chat with us on Whatsapp</button><br/><br/>
             </div>
-            <div className="col-md-6 text-center">
+            <div className="col-md-7 text-center">
+            <div className="image-container">
               <img className="img-responsive rounded" src="/images/main.png" alt="Main" data-aos="fade-up" data-aos-duration="2000" />
             </div>
-
+          </div>
           </div>
         </div>
       </section>
